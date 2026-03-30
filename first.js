a1600a148d85765397ca15f260b3e324d7a8a177
Akash Kumar <akdev63063@gmail.com>
2026-03-30 21:08:22 +0530

    All upload file

---
+++ first.js
@@ -0,0 +1,10 @@
+console.log("Akash Kumar")
+name = "Akash Kumar";
+Age = 24;
+Course = "Mca";
+percentage =83.33;
+console.log(name)
+console.log(Age)
+console.log(Course)
+console.log(percentage)
+

7efd6e120bc76e52a0dc438cef5796fbec09f163
Akash Kumar <akdev63063@gmail.com>
2026-03-30 22:47:55 +0530

    Variable define

---
+++ Variables.js
@@ -0,0 +1,23 @@
+// var: Old way to declare variables in JavaScript.
+// It is function-scoped, can be re-declared, and updated.
+
+
+// let: Modern way to declare variables (introduced in ES6).
+// It is block-scoped, cannot be re-declared, but can be updated.
+
+
+// const: Used to declare constant variables (fixed values).
+// It is block-scoped, cannot be re-declared or updated.
+var nam= "Akash";
+console.log(nam);
+var name1 = "Akash Ji";
+console.log(name1);
+let name = "Akash Kumar Jangda";
+console.log(name);
+let a;
+console.log(a);
+a = 55;
+console.log(a);
+const value = 10;
+console.log(value);
+

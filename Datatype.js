0090a48bb943b156abb9faa73a1fbe18afde1eb9
Akash Kumar <akdev63063@gmail.com>
2026-04-03 00:24:05 +0530

    Data types

---
+++ Datatype.js
@@ -0,0 +1,36 @@
+let x = 10;
+console.log(x);
+console.log(typeof(x));
+let fullName="Akash Kumar";
+console.log(fullName);
+console.log(typeof(fullName));
+isFollow = true;
+console.log(isFollow);
+console.log(typeof(isFollow));
+bn = BigInt(123);
+console.log(bn);
+console.log(typeof(bn));
+let y;
+console.log(y);
+console.log(typeof(y));
+let z = null;
+console.log(z);
+console.log(typeof(z));
+
+// Output
+
+/**
+ * 10
+number
+Akash Kumar
+string
+true
+boolean
+123n
+bigint
+undefined
+undefined
+null
+object
+ * 
+ */

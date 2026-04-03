5ab72935a8f2cec4ebf66052d665d38e9ea4afc6
Akash Kumar <akdev63063@gmail.com>
2026-04-04 00:11:46 +0530

    Assignment Operators

--- operators.js
+++ operators.js
@@ -1,4 +1,18 @@
 // Arithmatics Operators 
+/*
+------------------------------------------------------
+| Operator | Meaning             | Example  | Result |
+| -------- | ------------------- | -------- | ------ |
+| `+`      | Addition            | `10 + 5` | 15     |
+| `-`      | Subtraction         | `10 - 5` | 5      |
+| `*`      | Multiplication      | `10 * 5` | 50     |
+| `/`      | Division            | `10 / 5` | 2      |
+| `%`      | Modulus (remainder) | `10 % 3` | 1      |
+| `**`     | Exponent (power)    | `2 ** 3` | 8      |
+| `++`     | Increment           | `x++`    | x + 1  |
+| `--`     | Decrement           | `x--`    | x - 1  |
+------------------------------------------------------
+*/
 
 let a = 10;
 let b = 5;
@@ -15,6 +29,30 @@ console.log("a ** b =",a ** b);
 console.log("++a =",++a);
 // Decrement
 console.log("--a =",--a);
+
+// Assigment Operators 
+/*
+-----------------------------------------------------------
+| Operator | Meaning           | Example   | Result       |
+| -------- | ----------------- | --------- | ------------ |
+| `=`      | Assign value      | `x = 10`  | x becomes 10 |
+| `+=`     | Add & assign      | `x += 5`  | x = x + 5    |
+| `-=`     | Subtract & assign | `x -= 3`  | x = x - 3    |
+| `*=`     | Multiply & assign | `x *= 2`  | x = x * 2    |
+| `/=`     | Divide & assign   | `x /= 2`  | x = x / 2    |
+| `%=`     | Modulus & assign  | `x %= 3`  | x = x % 3    |
+| `**=`    | Exponent & assign | `x **= 2` | x = x²       |
+-----------------------------------------------------------
+*/
+x = 10;
+console.log("x += 5 = ",x += 5);
+console.log("x -= 5 = ",x -= 5);
+console.log("x *= 5 = ",x *= 5);
+console.log("x /= 5 = ",x /= 5);
+console.log("x %= 5 = ",x %= 5);
+console.log("x **= 5 = ",x **= 5);
+
+
 
 
 

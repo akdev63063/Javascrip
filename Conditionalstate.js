9feaefe2e353d666ed8d4dde7022b0fb868a6cd6
Akash Kumar <akdev63063@gmail.com>
2026-04-05 10:33:19 +0530

    Theory updated

---
+++ Conditionalstate.js
@@ -0,0 +1,11 @@
+/*
+Conditional statements in JavaScript are used to make decisions based on conditions (true/false).
+1. if statement --------------> Executes code if condition is true.
+2. if...else statement--------> Executes one block if true, another if false.
+3. if...else if...else--------> Checks multiple conditions.
+4. switch statement ----------> Used when you have multiple cases.
+5. Ternary Operator (Short if-else)-----> Used when you have multiple cases.
+*/
+
+// If - Statement
+ 

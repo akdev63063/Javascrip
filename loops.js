0f52c180b25871f80d4de659ea0ac3b7a67460ef
Akash Kumar <akdev63063@gmail.com>
2026-04-07 21:53:50 +0530

    Thory updated

---
+++ loops.js
@@ -0,0 +1,14 @@
+/*
+Loops in JavaScript are used to repeat a block of code multiple times until a condition is met.
+Quick Summary:
+for → fixed iterations
+while → condition-based
+do...while → runs at least once
+for...of → values (arrays/strings)
+for...in → object keys
+*********For Loop Syntax *************
+for (initialization; condition; increment/decrement) {
+    // code to execute
+}
+
+*/

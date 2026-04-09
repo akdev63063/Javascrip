6f6d5d515496de2922372e4c09304dfc29924cc1
Akash Kumar <akdev63063@gmail.com>
2026-04-09 23:43:31 +0530

    Complete,update

--- Practice/Pr5.js
+++ Practice/Pr5.js
@@ -1,2 +1,11 @@
 /*Create a game with start with any random game number.Ask the user to keep guess the game number until the users enters correct values  
 */
+
+let gameNum = 25;
+let userName = prompt("Guess The Game Number : ");
+while (userName !== gameNum)
+{
+    userName = prompt("You entered wrong Number ,Pleae try Again ");
+}
+console.log("Congratulation, You Entered Right number");
+

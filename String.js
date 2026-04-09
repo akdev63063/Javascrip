c99890fae32c49a7e83a4be96e768e3099014986
Akash Kumar <akdev63063@gmail.com>
2026-04-09 23:58:03 +0530

    String def and syntax and example

---
+++ String.js
@@ -0,0 +1,12 @@
+/*
+A string in JavaScript is used to store text data.
+A string is a sequence of characters used to represent text.
+example : let name = "Akash";
+
+Double Quotes
+let str1 = "Hello";
+2. Single Quotes
+let str2 = 'World';
+3. Backticks (Template Literals)
+let str3 = `Hello Akash`;
+*/

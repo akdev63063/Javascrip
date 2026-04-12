1a9ce23b2d43a18b5e8f4852d001ef2f04df5a35
Akash Kumar <akdev63063@gmail.com>
2026-04-12 23:50:43 +0530

    Print marks by looping

--- arrays.js
+++ arrays.js
@@ -8,3 +8,11 @@ let arr = [1, 2, 3, 4];
 */
 let arr = [10,20,30,40,50];
 console.log(arr);
+
+//Looping in arrays in JavaScript means accessing each element one by one. It’s very important for working with data.
+
+let marks = [45,50,65,96,45,74,55];
+for(let i = 0; i <= marks.length;i++)
+{
+    console.log(marks[i]);
+}

efa2bbbd1be7f0b2301036e9520b35be08a02cbe
Akash Kumar <akdev63063@gmail.com>
2026-04-12 22:44:00 +0530

    Solve Pr 6

---
+++ Practice/Pr6.js
@@ -0,0 +1,9 @@
+/*
+Prompt the user to enter their full name. Generate a username for them based on the input.Start username with @,followed by their full name and ending with the fullname length.
+Example : 
+*/
+
+username = prompt("Enter the username : ");
+start_with = "@"+username;
+len = username.length;
+console.log(start_with.concat(len));
--- String.js
+++ String.js
@@ -85,3 +85,12 @@ let str2 = "      Akash";
 
  // concatination 
  console.log(str1.concat(str2));
+/*
+Prompt the user to enter their full name. Generate a username for them based on the input.Start username with @,followed by their full name and ending with the fullname length.
+Example : 
+*/
+
+username = prompt("Enter the username : ");
+start_with = "@"+username;
+len = username.length;
+console.log(start_with.concat(len));

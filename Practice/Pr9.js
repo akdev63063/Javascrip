cf794542bcf06372c9f2301fa532ffec54528902
Akash Kumar <akdev63063@gmail.com>
2026-04-13 22:53:49 +0530

    Practice no 9 with solution

---
+++ Practice/Pr9.js
@@ -0,0 +1,16 @@
+/*
+For a given array with prices of 5 items -> [250,645,300,900,50]
+all items have an offer of 10% OFF on them. Change the array to store final price after apply offer. 
+*/
+let items = [250,645,300,900,50];
+let sum = 0;
+for (let itm of items)
+{
+    sum += itm;
+}
+discount = sum * 0.1;
+let price_after_dis=sum-discount;
+console.log(`Total price of all Items : ${sum}`);
+console.log(`Total Discount : ${discount}`);
+console.log(`Total price after 10% Discount : ${price_after_dis}`)
+

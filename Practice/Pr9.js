f1f924cac3bb1311d5736c21cafbe12c8192147c
Akash Kumar <akdev63063@gmail.com>
2026-04-13 23:41:41 +0530

    Practice no 9 with solution with another method

--- Practice/Pr9.js
+++ Practice/Pr9.js
@@ -4,8 +4,10 @@ all items have an offer of 10% OFF on them. Change the array to store final pric
 */
 let items = [250,645,300,900,50];
 let sum = 0;
+
 for (let itm of items)
 {
+
     sum += itm;
 }
 discount = sum * 0.1;
@@ -13,4 +15,15 @@ let price_after_dis=sum-discount;
 console.log(`Total price of all Items : ${sum}`);
 console.log(`Total Discount : ${discount}`);
 console.log(`Total price after 10% Discount : ${price_after_dis}`)
+
+// Example no 2 
+let nms = [250,645,300,900,50];
+let i= 0;
+for(let nm of nms)
+{
+    let offer = nm/10;
+    nms[i] = nms[i] - offer;
+    console.log(nms[i]);
+    i++;
+}
 

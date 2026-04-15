bceeb36b0f321a96593ce50ebb9333ff1b57df99
Akash Kumar <akdev63063@gmail.com>
2026-04-15 23:51:03 +0530

    Question secend complete of Pr10

--- Practice/Pr10.js
+++ Practice/Pr10.js
@@ -2,3 +2,10 @@
 Create an array to store companies- > ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"]
 */
 let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+
+/*
+Remove the first Company from the array
+*/
+let remove_fc = companies.shift();
+console.log(`Remove First Company : ${remove_fc}`);
+console.log(`Print array after Remove: ${companies}`);

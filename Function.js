8a070014f2b52d522475bf3e4f1e86584c471eec
Akash Kumar <akdev63063@gmail.com>
2026-04-16 18:18:39 +0530

    Functions defination and types with Example

---
+++ Function.js
@@ -0,0 +1,57 @@
+/*
+A function in JavaScript is a block of code that performs a specific task and runs when it is called.
+
+🔹 1. Basic Syntax of Function
+function functionName() {
+    // code to execute
+}
+Example:
+function greet() {
+    console.log("Hello World");
+}
+
+greet(); // calling the function
+🔹 2. Function with Parameters
+
+You can pass values (inputs) to a function.
+
+function greet(name) {
+    console.log("Hello " + name);
+}
+
+greet("Akash");
+🔹 3. Function with Return Value
+
+Functions can return a result using return.
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 3);
+console.log(result); // 8
+🔹 4. Function Expression
+
+Function stored in a variable.
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+console.log(multiply(4, 5));
+🔹 5. Arrow Function (Modern Way)
+
+Short syntax introduced in ES6.
+
+const sum = (a, b) => {
+    return a + b;
+};
+Short form:
+const sum = (a, b) => a + b;
+🔹 6. Types of Functions in JS
+Normal Function
+Anonymous Function
+Arrow Function
+Callback Function
+Recursive Function
+*/
